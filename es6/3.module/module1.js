fa1e4807e210f92947e0c0774e5f999bbb5afce6
wangliuyong <1355498705@qq.com>
2019-03-06 14:59:49 +0800

    module 模块化笔记

---
+++ es6/3.module/module1.js
@@ -0,0 +1,12 @@
+//模块:第三方（npm install） 引入不用 ‘./’；自定义的要用‘./’
+
+//使用解构赋值获取导入的内容
+//import会变量提升，但一般开发都会写在最上面
+import {name,age} from  './module2.js'
+//一次性导入所有
+import * as obj from './module2.js'
+//或者使用  export default {} 导出，也可以使用import一次性导入
+
+console.log(name);
+console.log(obj);
+
---
+++ es6/3.module/module2.js
@@ -0,0 +1,4 @@
+export let name=`wangliuyong`
+export let age=18;
+
+//导出形式 {name,age}对象的形式，需要使用解构赋值接收
