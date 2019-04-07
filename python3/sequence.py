1524fd42993739fc0e45891522c70f317854a55f
wly <1355498705@qq.com>
2019-04-07 15:28:37 +0800

    list tuple str 公共的用法

---
+++ python3/sequence.py
@@ -0,0 +1,15 @@
+name = "wang"
+lis = [9, 2, 8, 4]
+lis1 = [5,8,6,6]
+
+list()
+tuple()
+str()
+max([1,25,555])
+max([1,25,555])
+sum((1,2,3),10)
+sorted(((3,2,1)))
+reversed() #返回一个迭代器对象，需要用list()转化成列表
+list(reversed(name))
+enumerate() #返回（index,item）
+zip()
