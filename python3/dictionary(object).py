8cd6cfa47003b5ed8c109076b04532ed6671beb0
wly <1355498705@qq.com>
2019-04-08 12:42:18 +0800

    add dict some use

--- python3/dictionary(object).py
+++ python3/dictionary(object).py
@@ -8,3 +8,12 @@ dic['xingbie'] = 'man'
 del dic['Name'] # 删除键 'Name'
 dic.clear()     # 清空字典
 del dic         # 删除字典
+
+dic.keys()
+dic.items()
+dic.values
+dic.get()
+b=dic.copy() #浅拷贝,另存了一份，不是直接复制地址
+c=b.update(b)
+
+#  set集合
