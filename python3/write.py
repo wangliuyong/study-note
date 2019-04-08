200f431dedd14e8d55cc5ccbb3c4a029492a8ae1
wly <1355498705@qq.com>
2019-04-08 14:53:30 +0800

    封装成函数

--- python3/write.py
+++ python3/write.py
@@ -1,46 +1,30 @@
+def save_file(name,count,value):
+    file_name = './test/' + name + str(count) + '.txt'
+    f = open(file_name,'w',encoding='utf8')
+    f.writelines(value)
+    f.close()
 
 
-f = open('./example.txt',encoding='utf8')
+def split_file(_file):
+    f = open(_file,encoding='utf8')
+    san = []
+    si = []
+    count = 1
+    for each_line in f:
+        if each_line[:6] != '======':
+            lineArr = each_line.split('：')
+            print(lineArr)
+            name = lineArr[0]
+            say = lineArr[1].rstrip()
+            if name == '张三':
+                san.append(say)
+            elif name == '李四':
+                si.append(say)
+        else:
+            save_file('boy',count,san[count-1:])
+            save_file('girl',count,si[count-1:])
+            count += 1
+    print('san: \n',san,' \n','si: \n',si)
+    f.close()
 
-san = []
-si = []
-count = 1
-for each_line in f:
-    
-    if each_line[:6] != '======':
-        lineArr = each_line.split('：')
-        print(lineArr)
-        name = lineArr[0]
-        say = lineArr[1].rstrip()
-        if name == '张三':
-            san.append(say)
-        elif name == '李四':
-            si.append(say)
-    else:
-        boy = './test/boy' + str(count) + '.txt'
-        girl = './test/girl' + str(count) + '.txt'
-        save_boy = open(boy,'w',encoding='utf8')
-        save_boy.writelines(san[count-1:])
-        save_boy.close()
-        save_girl = open(girl,'w',encoding='utf8')
-        save_girl.writelines(si[count-1:])
-        count += 1
-        save_girl.close()
-
-print('san: \n',san,' \n','si: \n',si)
-
-f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-f.close()
+split_file('example.txt')
